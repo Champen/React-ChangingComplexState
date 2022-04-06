94581bf7766c3f66d0604d4f5379cf804e6f0acd
Champen <64540361+Champen@users.noreply.github.com>
2022-04-06 15:05:18 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -51,6 +51,24 @@ function App() {
       }
     });
   }
+  
+  
+  //-----A shorter way to write the handleChange function which does the same by using spread Operator-----//
+  
+  function handleChange(event) {
+    const { name, value } = event.target;
+
+    setContact((prevValue) => {
+      return {
+        //Returning on object with all the items from "prevValue"
+        //And adding the new key value pair ([name] : value) name of the inputfield and assigning the changed value to ir
+        ...prevValue,
+        [name]: value
+      };
+    });
+  }
+  
+  //-------------------------------------------------------------//
 
   return (
     <div className="container">
